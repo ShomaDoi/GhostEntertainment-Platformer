13bf027e9de1429ebc65141a8f76c9b894d82245
Ivan Zarkov <57815923+Ivan02qqq@users.noreply.github.com>
2019-11-16 01:11:53 +0100

    Publish Game
    Second Update

---
+++ Assets/Script/PitController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitController : MonoBehaviour{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.instance.player.transform.position = GameManager.instance.checkpointPosition;
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Controller script")]
+    public CharacterController2D controller;
+    [Space]
+
+    [Header("Player animator")]
+    public Animator animator;
+    [Space]
+
+    [Header("Running speed")]
+    public float runSpeed = 40f;
+    [Space]
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump") && CharacterController2D.instance.jumpNumber <= CharacterController2D.instance.maxJumpNumber)
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+        CharacterController2D.instance.jumpNumber = 0;
+    }
+}
