79f547a3e20abc4a4c3132d1f7b1a6c557565330
VladanS <49107047+vladanms@users.noreply.github.com>
2020-03-24 21:39:29 +0100

    Falling spikes
    #Unity 2019.3.0.4a

---
+++ Assets/Script/Controllers/Enemies/FallingSpikeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpikeController : MonoBehaviour
+{
+    public GameObject player, spike;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player.GetComponents<Collider2D>()[0]);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            {
+            spike.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+
+    }
+}
